986c005d2e8bd37a5864487b4bbe15a713961c57
Alejandro Villar Rubio <alvill04@ucm.es>
2022-02-17 18:11:11 +0100

    Added project and final game

---
+++ Vuela/Assets/uAdventure/Editor/EffectEditor/ConditionEditor/ConditionEditors/VarConditionEditor.cs
@@ -0,0 +1,93 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+
+using uAdventure.Core;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+
+namespace uAdventure.Editor
+{
+    public class VarConditionEditor : ConditionEditor
+    {
+        private VarCondition condition = new VarCondition("", 4, 0);
+
+        private static readonly Dictionary<int, string> stateToString = new Dictionary<int, string>
+        {
+            { VarCondition.VAR_GREATER_THAN, " > " }, 
+            { VarCondition.VAR_GREATER_EQUALS_THAN, " ≥ " }, 
+            { VarCondition.VAR_EQUALS, " = " }, 
+            { VarCondition.VAR_LESS_THAN, " < " }, 
+            { VarCondition.VAR_LESS_EQUALS_THAN, " ≤ " },
+            { VarCondition.VAR_NOT_EQUALS, " ≠ " }
+        };
+
+        private readonly Dictionary<int, int> indexToState = new Dictionary<int, int>();
+        private readonly Dictionary<int, int> stateToIndex = new Dictionary<int, int>();
+        private readonly string[] valueStrings;
+        private readonly string name = TC.get("Vars.Var");
+
+        public VarConditionEditor()
+        {
+            var stateToStringEnumerator = stateToString.GetEnumerator();
+            for(int i = 0; i < stateToString.Count; i++)
+            {
+                stateToStringEnumerator.MoveNext();
+                indexToState.Add(i, stateToStringEnumerator.Current.Key);
+                stateToIndex.Add(stateToStringEnumerator.Current.Key, i);
+            }
+            valueStrings = stateToString.Values.ToArray();
+
+            if (Available)
+            {
+                condition = new VarCondition(Controller.Instance.VarFlagSummary.getVars()[0], 4, 0);
+            }
+        }
+
+        public void draw(Condition c)
+        {
+            condition = c as VarCondition;
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (Available)
+            {
+                var vars = Controller.Instance.VarFlagSummary.getVars();
+                var index = Mathf.Max(0, Array.IndexOf(vars, c.getId()));
+                condition.setId(vars[EditorGUILayout.Popup(index >= 0 ? index : 0, vars)]);
+                condition.setState(indexToState[EditorGUILayout.Popup(stateToIndex[condition.getState()], valueStrings)]);
+                condition.setValue(int.Parse(EditorGUILayout.TextField(condition.getValue().ToString())));
+            }
+            else
+            {
+                EditorGUILayout.HelpBox(TC.get("Condition.Var.Warning"), MessageType.Error);
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        public bool manages(Condition c)
+        {
+            return c.GetType() == condition.GetType();
+        }
+
+        public string conditionName()
+        {
+            return name;
+        }
+
+        public Condition InstanceManagedCondition()
+        {
+            return new VarCondition("", 4, 0);
+        }
+
+        public bool Collapsed { get; set; }
+        public Rect Window { get; set; }
+        public bool Available { get { return Controller.Instance.VarFlagSummary.getVars().Length > 0; } }
+
+
+    }
+}
---
+++ Vuela/Assets/uAdventureGeo/Editor/Util/DropDown.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using uAdventure.Core;
+
+public class DropDown {
+
+    private const float labelSize = 150;
+
+    public string Value;
+    public string Label;
+    public List<string> Elements;
+    
+    private Drop drop;
+    private bool selected;
+    private string nextValue;
+    private EditorWindow myWindow;
+
+    public DropDown(string label)
+    {
+        Label = label;
+        Drop.OnOptionSelected = (index, option) =>
+        {
+            nextValue = option;
+            selected = true;
+        };
+    }
+
+
+    public bool DoLayout()
+    {
+        return DoLayout(null);
+    }
+
+
+    public bool DoLayout(GUIStyle style)
+    {
+        // Creating Layout element
+        GUI.SetNextControlName(Label);
+        if (style == null)
+        {
+            Value = EditorGUILayout.TextField(Label, Value);
+        }
+        else
+        {
+            Value = EditorGUILayout.TextField(Label, Value, style);
+        }
+
+        if(drop != null && drop.editorWindow == EditorWindow.focusedWindow)
+        {
+            myWindow.Focus();
+            GUI.FocusControl(Label);
+        }
+
+        // Calculating dropdown scroll rect
+        var addressRect = GUILayoutUtility.GetLastRect();
+
+
+        var r = selected;
+        if (selected)
+        {
+            this.Elements.Clear();
+            selected = false;
+            this.Value = nextValue;
+        }
+        else
+        {
+            // If focused show
+            if (Event.current.type == EventType.Repaint && GUI.GetNameOfFocusedControl() == Label &&
+                Elements != null && Elements.Count > 0)
+            {
+                Drop.Elements = Elements;
+                
+                if (drop == null)
+                {
+                    myWindow = EditorWindow.focusedWindow;
+                    drop = Drop.ShowAt(addressRect);
+                }
+                drop.Repaint();
+            }
+
+            if (GUI.GetNameOfFocusedControl() != Label && drop != null)
+            {
+                drop.editorWindow.Close(); 
+            }
+        }
+
+        return r;
+    }
+    
+}
+public class Drop : PopupWindowContent
+{
+    private Vector2 scrollPos;
+    private GUISkin dropdownskin;
+
+    public delegate void OnOptionSelectedDelegate(int index, string option);
+
+    public static OnOptionSelectedDelegate OnOptionSelected;
+
+    public static List<string> Elements { get; set; }
+
+    public static Drop ShowAt(Rect rect)
+    {
+        var drop = new Drop();
+        PopupWindow.Show(rect, drop);
+        return drop;
+    }
+
+    public override void OnOpen()
+    {
+        dropdownskin = Resources.Load<GUISkin>("DropdownSkin");
+    }
+
+    public override void OnGUI(Rect rect)
+    {
+        // Show Scrollview
+        using (new GUIUtil.SkinScope(dropdownskin))
+        using (var scroll = new GUILayout.ScrollViewScope(scrollPos, false, false, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+        {
+            scrollPos = scroll.scrollPosition;
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                if (GUILayout.Button(Elements[i]))
+                {
+                    if (OnOptionSelected != null)
+                    {
+                        OnOptionSelected(i, Elements[i]);
+                    }
+                    GUI.FocusControl(null);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void Repaint()
+    {
+        this.editorWindow.Repaint();
+    }
+}
